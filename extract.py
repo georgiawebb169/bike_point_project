f4e86c4d6aed89b12b19f71c0ee7b7cf12c4b74b
georgiawebb169 <georgia.ml.webb@gmail.com>
2026-07-08 15:41:59 +0100

    added rety logic

--- extract.py
+++ extract.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import time 
 import logging 
 
-url = 'https://api.tfl.gov.uk/BikePoint/'
+url = 'https://api.tfl.gov.uk/BikePoin/'
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 timestamp = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
@@ -14,10 +14,21 @@ max_retry = 5
 attempt = 0
 delay = 10
 
-
-response = requests.get(url)
-data = response.json()
-
-with open(filename,'w') as file:
-        json.dump(data,file)
+while attempt < max_retry:
+    response = requests.get(url)
+    status = response.status_code
+    if 200<= status < 300:
+        data = response.json()
+        with open(filename,'w') as file:
+            json.dump(data,file)
+        print('yippee')
+        break
+    elif status<= 100 or status>=500:
+        time.sleep(delay)
+        print('retrying')
+        attempt+=1
+    else:
+        print('fix something')
+        print(status)
+        break
 
